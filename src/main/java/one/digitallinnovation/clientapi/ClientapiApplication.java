f236c619dca1c6855216782e198b4b4e7449231b
lgmro <lucasgmmro@gmail.com>
2021-06-30 19:37:03 -0300

    Create project

---
+++ src/main/java/one/digitallinnovation/clientapi/ClientapiApplication.java
@@ -0,0 +1,14 @@
+package one.digitallinnovation.clientapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientapiApplication.class, args);
+	}
+
+
+}
